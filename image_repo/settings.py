3cf644ec60bb634b5d4cfa64860f0a78623381cd
surabhi-99 <surabhithakur.33@gmail.com>
2022-12-17 20:06:19 +0530

    testing

--- image_repo/settings.py
+++ image_repo/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'image_repo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'iert@1234',
+        'HOST': 'iert-server.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
 
